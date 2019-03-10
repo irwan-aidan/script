ebea7ba99285fa949bb7faf33e09876c136e9513
leha98 <41480901+leha98@users.noreply.github.com>
2019-03-11 00:28:22 +0700

    Update google.sh

--- google.sh
+++ google.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/bash
-chmod a+x /usr/bin/google-chrome
 echo "Google Chrome run with user root Kali Linux"
 sed -i '$d' /opt/google/chrome/google-chrome
 echo -e 'exec -a "$0" "$HERE/chrome" "$@" --user-data-dir %U --test-type --no-sandbox --disable-rich-notifications' >> /opt/google/chrome/google-chrome
