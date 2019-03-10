1f61adb9a000de335235a0bb7d4baf1c7a8d601c
leha98 <41480901+leha98@users.noreply.github.com>
2019-03-11 00:39:27 +0700

    Delete google-chrome.sh

--- google-chrome.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/bash
-sed -i '$d' /opt/google/chrome/google-chrome
-echo -e 'exec -a "$0" "$HERE/chrome" "$@" --user-data-dir %U --test-type --no-sandbox --disable-rich-notifications' >> /opt/google/chrome/google-chrome
-exit;
