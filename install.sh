8bde8ac0fe8b830efcae3edb6684645a69228230
leha198 <41480901+leha198@users.noreply.github.com>
2019-11-03 14:04:10 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,31 +1,31 @@
-#!bin/bash
-
-#Cai dat Cyberpanel
-install(){
+#!/bin/bash
+cyber(){
 	wget -O cyberpanel.sh https://cyberpanel.net/install.sh
 	chmod +x cyberpanel.sh
 	./cyberpanel.sh -v ols -c 1 -a 1 -p r
 }
-
 #Thay doi gia tri PHP
-change(){
+centos(){
 	cd /usr/local/lsws/
 	find ./  -type f -name "php.ini" | xargs sed -i -e 's/2M/1024M/g' -e 's/128M/256M/g' -e 's/8M/1024M/g'
 	echo "max_allowed_packet=1024M" >> /etc/my.cnf
-	service lscpd restart
+	service lscpd restart && service mysql restart
 }
 
-delete(){
-	rm -rf install* cyberpanel*
+ubuntu(){
+	cd /usr/local/lsws/
+	find ./  -type f -name "php.ini" | xargs sed -i -e 's/2M/1024M/g' -e 's/128M/256M/g' -e 's/8M/1024M/g'
+	echo "max_allowed_packet=1024M" >> /etc/mysql/my.cnf
+	service lscpd restart && service mysql restart
 }
 
-if	grep -q -i "release 7" /etc/redhat-release ; then
-	echo "Update CentOS moi nhat"
-	sleep 2 && yum update -y
-	install
-	change
-	sleep 2 && delete
-else
-	echo "Cyberpanel khong ho tro hien tai:`cat /etc/*-release`"
-	delete
+
+if [ -f /etc/redhat-release ]; then
+  yum update -y
+  cyber
+  rm -rf install* cyber*
+else [ -f /etc/lsb-release ]; then
+  apt update -y && apt upgrade -y
+  cyber
+  rm -rf install* cyber*
 fi
