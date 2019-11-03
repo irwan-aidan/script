01c3de4ec8a89851bad377a11b921d6c70e1daae
leha198 <41480901+leha198@users.noreply.github.com>
2019-11-03 14:48:37 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,15 +11,12 @@ centos(){
 	echo "max_allowed_packet=1024M" >> /etc/my.cnf
 	service lscpd restart && service mysql restart
 }
-
 ubuntu(){
 	cd /usr/local/lsws/
 	find ./  -type f -name "php.ini" | xargs sed -i -e 's/2M/1024M/g' -e 's/128M/256M/g' -e 's/8M/1024M/g'
 	echo "max_allowed_packet=1024M" >> /etc/mysql/my.cnf
 	service lscpd restart && service mysql restart
 }
-
-
 if [ -f /etc/redhat-release ]; then
   yum update -y
   cyber
