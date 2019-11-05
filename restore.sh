4b8542009a7c41899ea5b72090d764e57fba252e
leha198 <41480901+leha198@users.noreply.github.com>
2019-11-05 16:15:26 +0700

    Update restore.sh

--- restore.sh
+++ restore.sh
@@ -13,7 +13,7 @@ user=$(grep DB_USER $config | awk -F\' '{print$4}')
 db=$(grep DB_NAME $config | awk -F\' '{print$4}')
 pass=$(grep DB_PASSWORD $config | awk -F\' '{print$4}')
 unzip -q *.zip 
-mv -f home/*/$domain/DocumentRoot/ /home/*/$domain/
+mv -f $domain/DocumentRoot/ /home/*/$domain/
 mysql -u $user -p$pass $db < backup.sql
 if [ "$?" -eq 0 ] ; then
     echo "======================================="
